37c33b392ce51a04af44911269db060197154066
Poongothai-R <107067696+Poongothai-R@users.noreply.github.com>
2022-12-29 20:07:55 +0530

    Project section created

---
+++ src/App.jsx
@@ -0,0 +1,31 @@
+import './App.css';
+import Header from "./Components/Header";
+import Projects from "./Scroll_pages/Projects";
+import Hero from "./Scroll_pages/Hero";
+import About from "./Scroll_pages/About";
+import Modal from "./Components/Modal";
+import {useState} from "react";
+import Tech from "./Scroll_pages/Tech";
+
+import './Styles/variable.css';
+import Contact from "./Scroll_pages/Contact";
+
+
+function App() {
+    const [showModal, setShowModal] = useState(null);
+  return (
+    <div className="App">
+      <Header/>
+        <Hero/>
+      <About/>
+
+        <Projects showModal={showModal}/>
+        <Tech/>
+        <Contact/>
+        <Modal showModal={showModal} setShowModal={setShowModal} />
+
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Modal.jsx
@@ -0,0 +1,21 @@
+import ReactDOM from "react-dom";
+import '../Styles/modal.css'
+
+export default function Modal({ showModal, setShowModal }) {
+    //safeguard
+    if (showModal === null) return null;
+
+    return ReactDOM.createPortal(
+        <>
+            <div className="overlay_style">
+                <div className="modal_style">
+                    <div className="btn_cancel">
+                        <button onClick={() => setShowModal(null)}>X</button>
+                    </div>
+                    {showModal}
+                </div>
+            </div>
+        </>,
+        document.getElementById("portal")
+    );
+}
